6e050bee88f287ac69f7a9b544fad6e900a80539
Miliya Immatty Lazar <miliyaninu@gmail.com>
2024-09-20 16:20:52 +1000

    completed

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(a, b) {
+  return a / b;
+};
+divide(2000, 100)
+const square = (x) => {
+  return x * x;
+};
+square(5)
+const add = (a, b) => {
+  return a + b;
+};
+add(8,7)
+
